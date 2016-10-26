a6c30d66ccb9c2d572bb2404e7ad55478e2f357c
kcmuths <kcmuths@ccs.neu.edu>
2016-10-26 17:52:58 -0400

    Bug in deleting comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,15 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params) #create and save the comment to a particular article
     redirect_to article_path(@article)
   end
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(comment_params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
   private
   def comment_params
-    params.require( :comment).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, #used to validate the data that is sent to model
             length: {minimum: 5 } # make sure the title length is atleast 5 chars
 end
